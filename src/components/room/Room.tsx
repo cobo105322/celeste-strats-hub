e9d1fdbb984ebd5435d582bc500a1741545fb398
Daniel Cobo Borrega <dcobobor@nasertic.es>
2022-03-14 14:48:12 +0100

    Created room component with basic design

--- src/components/room/Room.tsx
+++ src/components/room/Room.tsx
@@ -1,36 +1,161 @@
+import './Room.css';
+
 import React from 'react';
-import { ChapterTree, Checkpoint, Side } from '../../models/interfaces/ChapterTree';
-import {useParams } from "react-router-dom";
-interface ComponentProps{
-    chapterTree: ChapterTree,
-    match: any
+import { Chapter, ChapterTree, Checkpoint, iRoom, Side } from '../../models/interfaces/ChapterTree';
+import { Link, RouteComponentProps, useParams } from "react-router-dom";
+import { Strat } from '../../models/interfaces/Strat';
+import { FilterState } from '../../models/interfaces/FilterState';
+import ReactLoading from 'react-loading';
+import { APIService } from '../../utils/ApiService';
+import { EnumAPIEndpoint } from '../../models/enums/EnumAPIEndpoint';
+import { Panel } from 'primereact/panel';
+import { RoomLogic } from './RoomLogic';
+
+interface MatchParams {
+  id: string
 }
 
-interface ComponentState{
-    chapter: string,
-    side: Side,
-    checkpoint: Checkpoint,
-    room: Room
+
+
+interface ComponentProps {
+  chapterTree: ChapterTree,
+  filters: FilterState,
+  params: MatchParams
 }
 
-export default class Room extends React.PureComponent<any, ComponentProps> {
-    constructor(props: any) {
-      super(props);           
-      
-      console.log(this.props)
+interface ComponentState {
+  roomStrats: Strat[]
+}
+
+export default class Room extends React.Component<ComponentProps, ComponentState> {
+
+  private roomLogic: RoomLogic;
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      roomStrats: null
     }
 
-    componentDidMount(): void {
+    this.roomLogic = new RoomLogic(this.props.params.id, this.props.chapterTree);
 
-    }
-  
-    render() {
-        
-      return (
-        <div className="room">
-            HEWWO THIS IS RWOOM NUMBEW 
-        </div>
-      );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.params.id !== prevProps.params.id) {
+      this.initialize();
     }
   }
-  
+
+
+  componentDidMount(): void {
+    this.initialize();
+  }
+
+  initialize() {
+    this.roomLogic = new RoomLogic(this.props.params.id, this.props.chapterTree);
+    setTimeout(() => {
+      APIService.APICall(EnumAPIEndpoint.GET_STRATS, { room_id: this.props.params.id }).then((strats: Strat[]) => {
+        this.setState({ roomStrats: strats })
+      })
+    }, 1); //Simulating load
+  }
+
+  getLink(debugId: string) {
+    let fullId = this.roomLogic.room.id.substring(0, 3) + debugId;
+    //We get the first part of the identifier
+    return <Link to={"/room/" + fullId}>{debugId}</Link>
+  }
+
+  renderRoomImage() {
+    return (
+      <div className="col-12 lg:col-6">
+        <h2>{`${this.roomLogic.chapter.name} ${this.roomLogic.chapter.chapter_no != 9 ? this.roomLogic.side.name + ' side' : ''} - ${this.roomLogic.checkpoint.name}`}</h2>
+        <img style={{ width: '320px' }} src={`${process.env.PUBLIC_URL}/img/room/${this.roomLogic.room.id}.png`} />
+      </div>)
+  }
+
+  renderRoomData() {
+    return (
+      <div className="col-12 lg:col-6">
+        <h3>Room Data</h3>
+        <div className="grid roomdata-row">
+          <div className="col-4 roomdata-title">
+            Chapter
+            </div>
+          <div className="col-8 roomdata-data">
+            {this.roomLogic.chapter.name}
+          </div>
+        </div>
+        <div className="grid roomdata-row">
+          <div className="col-4  roomdata-title">
+            Side
+            </div>
+          <div className="col-8 roomdata-data">
+            {this.roomLogic.side.name}
+          </div>
+        </div>
+        <div className="grid roomdata-row">
+          <div className="col-4  roomdata-title">
+            Checkpoint
+            </div>
+          <div className="col-8 roomdata-data">
+            {this.roomLogic.checkpoint.name}
+          </div>
+        </div>
+        <div className="grid roomdata-row">
+          <div className="col-4  roomdata-title">
+            Debug id
+            </div>
+          <div className="col-8 roomdata-data">
+            {this.roomLogic.room.debug_id}
+          </div>
+        </div>
+        <div className="grid roomdata-row">
+          <div className="col-4  roomdata-title">
+            Adjacent
+            </div>
+          <div className="col-8">
+            {this.roomLogic.room.linked.map((link, i) => <label>{i !== 0 && ', '}{this.getLink(link)}</label>)}
+          </div>
+        </div>
+
+      </div>)
+  }
+
+  renderStratsTable(){
+    let strats = this.state.roomStrats;
+    return (
+      <>
+      <h2>Strats</h2>
+      <div>TODO</div>
+      </>
+    )
+  }
+
+  render() {
+    if (!this.roomLogic.roomExists()) {
+      return <div>Error - room not found.</div>
+    }
+    if (!this.state.roomStrats) {
+      return (
+        <div className="loading">
+          <p>Loading...</p>
+          <div style={{ display: 'flex', justifyContent: 'center' }}> <ReactLoading type="spin" color="#000000" /> </div>
+        </div>)
+    }
+    
+    return (
+
+      <div className="room">
+        <Panel>
+        <div className="grid">
+          {this.renderRoomImage()}
+          {this.renderRoomData()}
+        </div>
+        {this.renderStratsTable()}
+      
+      </Panel>
+      </div >
+    );
+  }
+}
---
+++ src/components/room/RoomLogic.ts
@@ -0,0 +1,48 @@
+import { Chapter, Side, Checkpoint, iRoom } from './../../models/interfaces/ChapterTree';
+import { ChapterTree } from "../../models/interfaces/ChapterTree";
+interface FullRoom {
+    chapter: Chapter;
+    side: Side;
+    checkpoint: Checkpoint;
+    room: iRoom;
+}
+export class RoomLogic {
+    private fullroom: FullRoom = null;
+    constructor(roomId: string, chapterTree: ChapterTree) {
+        this.fullroom = this.getRoom(roomId, chapterTree);
+    }
+
+    get chapter() { return this.fullroom.chapter; }
+    get side() { return this.fullroom.side; }
+    get checkpoint() { return this.fullroom.checkpoint; }
+    get room() { return this.fullroom.room; }
+
+    private getRoom(roomId: string, chapterTree: ChapterTree): FullRoom {
+        let tree: ChapterTree = chapterTree;
+        for (let x in tree) {
+            let chapter: Chapter = tree[x];
+            for (let i = 0; i < chapter.sides.length; i++) {
+                let side: Side = chapter.sides[i];
+                for (let j = 0; j < side.checkpoints.length; j++) {
+                    let checkpoint: Checkpoint = side.checkpoints[j];
+                    for (let k = 0; k < checkpoint.rooms.length; k++) {
+                        let room = checkpoint.rooms[k];
+                        if (room.id === roomId) {
+                            return {
+                                chapter: chapter,
+                                side: side,
+                                checkpoint: checkpoint,
+                                room: room
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public roomExists() {
+        return this.fullroom !== null;
+    }
+}
