335e4b4689abed1083fb2ad0f0b3964dc6793e51
Daniel Cobo Borrega <dcobobor@nasertic.es>
2022-03-14 14:48:44 +0100

    Created strat component intended to render a strat

---
+++ src/components/strat/Strat.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class Strat extends React.PureComponent<any, any> {
+    constructor(props: any) {
+      super(props);     
+    }
+  
+    render() {      
+      return (
+        <div className="about">
+          TODO
+        </div>
+      );
+    }
+  }
+  
