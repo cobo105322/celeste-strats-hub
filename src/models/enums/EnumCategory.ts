0d2a94180300bc99022380a6893f5c54359c317a
cobo105322 <danielociruelo@gmail.com>
2022-03-17 14:45:24 +0100

    Cambiados los enum de categoria y dificultad a un identificador numerico

--- src/models/enums/EnumCategory.ts
+++ src/models/enums/EnumCategory.ts
@@ -1,8 +1,10 @@
 export enum EnumCategory{
-    ANY_PERCENT = "any",
-    ALL_RED_BERRIES = "arb",
-    HUNDRED_PERCENT = "hundo",
-    ALL_A_SIDES = "aas",
-    ALL_B_SIDES = "abs",
-    ALL_C_SIDES = "acs"
+    ANY_PERCENT = 1,
+    ALL_RED_BERRIES = 2,
+    HUNDRED_PERCENT = 3,
+    ALL_CASSETTES = 4,
+    ALL_HEARTS = 5,
+    ALL_A_SIDES = 6,
+    ALL_B_SIDES = 7,
+    ALL_C_SIDES = 8
 }
