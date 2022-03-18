b4d07995218f8dd7895339ed066b09b490019836
cobo105322 <danielociruelo@gmail.com>
2022-03-18 10:35:41 +0100

    Created interfaces for difficulty and category

---
+++ src/models/interfaces/Category.ts
@@ -0,0 +1,6 @@
+import { EnumCategory } from './../enums/EnumCategory';
+
+export interface Category{
+    id: EnumCategory,
+    label: string
+}
---
+++ src/models/interfaces/Difficulty.ts
@@ -0,0 +1,6 @@
+import { EnumDifficulty } from "../enums/EnumDifficulty";
+
+export interface Difficulty{
+    id: EnumDifficulty,
+    label: string
+}
