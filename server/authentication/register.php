7a14a09af3386b1bbc5e972f217981d84bad2789
cobo105322 <danielociruelo@gmail.com>
2022-03-18 14:56:23 +0100

    Script for registering a new user

---
+++ server/authentication/register.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once('../config/config.php');
+include_once('../utils/dbutils.php');
+
+if(!isset($_POST["user"]) || !isset($_POST["password"]) || !isset($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+    header("Status: 404 Wrong Parameters");
+    header("HTTP/1.1 404 Wrong Parameters");
+    die();
+}
+
+$conn = new mysqli($dbconfig["servername"], $dbconfig["user"], $dbconfig["password"]);
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->select_db($dbconfig["dbname"]);
+
+
+//Hash the password with sha2
+$password = hash("sha256", $_POST["password"]);
+
+$stmt = $conn->prepare("INSERT INTO user (name, password, email) VALUES (?, ?, ?)");
+$stmt -> bind_param("sss", $_POST["user"], $password, $_POST["email"]);
+$stmt->execute();
+
+echo $conn->insert_id;
+
+?>
