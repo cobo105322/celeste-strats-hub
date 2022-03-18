7cb3d7ffeb84f0ac86e1b810bd5922a23f791a04
cobo105322 <danielociruelo@gmail.com>
2022-03-18 10:34:58 +0100

    Created functions to properly call the server

--- src/utils/ApiService.ts
+++ src/utils/ApiService.ts
@@ -1,18 +1,82 @@
 import { EnumAPIEndpoint } from "../models/enums/EnumAPIEndpoint";
 import { Strat } from "../models/interfaces/Strat";
 
-export class APIService{
+export enum EnumEnviroments {
+    LOCAL = 'local',
+    DESARROLLO = 'dev',
+    PRODUCCION = 'pro'
+}
 
-    private static getMockupStrats(room_id: string){
+
+export class APIService {
+
+    static readonly ENVIROMENT: EnumEnviroments = EnumEnviroments.LOCAL
+
+    private static getMockupStrats(room_id: string) {
         let stratsObject = require("../data/mockup-strats.json");
         let strats: Strat[] = stratsObject.strats;
-        return strats.filter(strat=>strat.room_id === room_id);
+        return strats.filter(strat => strat.room_id === room_id);
     }
 
-    static APICall(endpoint: EnumAPIEndpoint, parameters: { [key: string]: any}): Promise<any>{
-        //TODO - actually call the backend and recover the data. For now we are recovering and return the mockup strats        
-        if(endpoint === EnumAPIEndpoint.GET_STRATS && parameters["room_id"]){            
-            return Promise.resolve(this.getMockupStrats(parameters["room_id"]));
+    static readonly SERVER_URL = (APIService.ENVIROMENT == EnumEnviroments.LOCAL) ? 'http://localhost:8081/' : window.location.origin + '/';
+
+    static APICall(endpoint: EnumAPIEndpoint, parameters: { [key: string]: any }, method: string = 'post'): Promise<Response> {
+        let finalUrl: string;
+        let credentials: RequestCredentials = 'include';
+
+        finalUrl = this.SERVER_URL + endpoint;
+
+        //Crea un FormData y le anade los parametros recibidos
+
+        //Hace una llamada POST a la url
+        let resultFetch: Promise<Response>;
+        if (method.toUpperCase() == 'POST') {
+            let params = new FormData();
+            for (let key in parameters) {
+                params.append(key, parameters[key]);
+            }
+            resultFetch = fetch(finalUrl, {
+                method: method,
+                credentials: credentials,
+                headers: {},
+                body: params
+            });
+        } else { //asumamos GET de momento
+            let first = true;
+            for (let key in parameters) {
+                finalUrl += first ? '?' + key + '=' : '&' + key + '=';
+                first = false;
+                finalUrl += parameters[key];
+            }
+            resultFetch = fetch(finalUrl, {
+                method: method,
+                credentials: credentials,
+                headers: {}
+            });
         }
+
+        resultFetch.then((r) => {
+            if (r.status == 401) {
+                window.location.reload();
+            }
+        })
+        resultFetch.catch((error) => {
+            this.errorFetch(error, endpoint);
+        });
+        return resultFetch;
+    }
+
+    static GET_URL(url: EnumAPIEndpoint, parametros: { [key: string]: any }) {
+        let finalUrl = this.SERVER_URL + url;
+        let first = true;
+        for (let key in parametros) {
+            finalUrl += first ? `?${key}=${parametros[key]}` : `&${key}=${parametros[key]}`;
+            first = false;
+        }
+        return finalUrl;
+    }
+
+    static errorFetch(error: { message: string }, url: string) {
+        console.error("Error: ", error.message, "con ", url);
     }
 }
