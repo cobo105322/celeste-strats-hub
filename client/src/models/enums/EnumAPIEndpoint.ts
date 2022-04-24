4c3914f480894e55b32be7afdf20b6f9b999c996
cobo105322 <danielociruelo@gmail.com>
2022-04-24 11:17:30 +0200

    added new api endpoints to the enum

--- client/src/models/enums/EnumAPIEndpoint.ts
+++ client/src/models/enums/EnumAPIEndpoint.ts
@@ -1,4 +1,11 @@
 export enum EnumAPIEndpoint{
+    //STRATS
     GET_STRATS="api/getRoomStrats.php",
-    SUBMIT_STRAT="api/submitStrat.php"
+    SUBMIT_STRAT="api/submitRoomStrat.php",
+
+    //AUTH
+    LOGIN='authentication/login.php',
+    REGISTER='authentication/register.php',
+    LOGOUT='authentication/logout.php',
+    AUTOLOGIN='authentication/autologin.php'
 }
