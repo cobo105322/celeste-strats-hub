c49a8ea270400859c2ba67869fa13f216aee6dc9
cobo105322 <danielociruelo@gmail.com>
2022-04-24 11:15:34 +0200

    Moved the room details to it's own component

--- client/src/components/room/Room.tsx
+++ client/src/components/room/Room.tsx
@@ -12,6 +12,7 @@ import { Panel } from 'primereact/panel';
 import { RoomLogic } from './RoomLogic';
 import { Accordion, AccordionTab } from 'primereact/accordion';
 import { isThisTypeNode } from 'typescript';
+import RoomDetails from './RoomDetails';
 
 interface MatchParams {
   id: string
@@ -32,7 +33,7 @@ interface ComponentState {
 }
 
 export default class Room extends React.Component<ComponentProps, ComponentState> {
-  
+
   private roomLogic: RoomLogic;
   constructor(props: any) {
     super(props);
@@ -55,24 +56,23 @@ export default class Room extends React.Component<ComponentProps, ComponentState
     this.initialize();
   }
 
-  getInitialState(): ComponentState{
-    return  {
+  getInitialState(): ComponentState {
+    return {
       roomStrats: null,
       accordionIndex: 0,
       selectedStrat: null
     }
   }
 
-  initialize() {    
+  initialize() {
     this.roomLogic = new RoomLogic(this.props.params.id, this.props.chapterTree);
     this.setState(this.getInitialState());
-    setTimeout(() => {
-      APIService.APICall(EnumAPIEndpoint.GET_STRATS, { roomid: this.props.params.id }, "get").then((response: Response) => {
-        if(response.ok){
-          response.json().then((strats: Strat[])=>this.setState({ roomStrats: strats }));
-        }        
-      })
-    }, 1); //Simulating load
+    APIService.APICall(EnumAPIEndpoint.GET_STRATS, { roomid: this.props.params.id }, "get").then((response: Response) => {
+      if (response.ok) {
+        response.json().then((strats: Strat[]) => this.setState({ roomStrats: strats }));
+      }
+    })
+
   }
 
   getLink(debugId: string) {
@@ -81,7 +81,7 @@ export default class Room extends React.Component<ComponentProps, ComponentState
     return <Link to={"/room/" + fullId}>{debugId}</Link>
   }
 
-  getLinkById(id: string) {    
+  getLinkById(id: string) {
     //We get the first part of the identifier
     return <Link to={"/room/" + id}>{id}</Link>
   }
@@ -101,7 +101,7 @@ export default class Room extends React.Component<ComponentProps, ComponentState
         <div className="grid roomdata-row">
           <div className="col-4 roomdata-title">
             Chapter
-            </div>
+          </div>
           <div className="col-8 roomdata-data">
             {this.roomLogic.chapter.name}
           </div>
@@ -109,7 +109,7 @@ export default class Room extends React.Component<ComponentProps, ComponentState
         <div className="grid roomdata-row">
           <div className="col-4  roomdata-title">
             Side
-            </div>
+          </div>
           <div className="col-8 roomdata-data">
             {this.roomLogic.side.name}
           </div>
@@ -117,7 +117,7 @@ export default class Room extends React.Component<ComponentProps, ComponentState
         <div className="grid roomdata-row">
           <div className="col-4  roomdata-title">
             Checkpoint
-            </div>
+          </div>
           <div className="col-8 roomdata-data">
             {this.roomLogic.checkpoint.name}
           </div>
@@ -125,7 +125,7 @@ export default class Room extends React.Component<ComponentProps, ComponentState
         <div className="grid roomdata-row">
           <div className="col-4  roomdata-title">
             Debug id
-            </div>
+          </div>
           <div className="col-8 roomdata-data">
             {this.roomLogic.room.debug_id}
           </div>
@@ -133,7 +133,7 @@ export default class Room extends React.Component<ComponentProps, ComponentState
         <div className="grid roomdata-row">
           <div className="col-4  roomdata-title">
             Adjacent
-            </div>
+          </div>
           <div className="col-8">
             {this.roomLogic.room.linked.map((linked_id, i) => <label>{i !== 0 && ', '}{this.getLink(linked_id)}</label>)}
           </div>
@@ -142,8 +142,8 @@ export default class Room extends React.Component<ComponentProps, ComponentState
       </div>)
   }
 
-  
-  renderStratsTable(): JSX.Element{
+
+  renderStratsTable(): JSX.Element {
     if (!this.state.roomStrats) {
       return (
         <div className="loading">
@@ -153,41 +153,41 @@ export default class Room extends React.Component<ComponentProps, ComponentState
     }
     let filteredStrats: Strat[] = this.roomLogic.getFilteredStrats(this.state.roomStrats, this.props.filters);
     return <div className="grid">
-      {filteredStrats.map((strat,i)=>{
-        return <div key={"strat_"+i} className="col-12 lg:col-4 strat-preview-div">
-          <p onClick={()=>this.setState({selectedStrat: strat, accordionIndex: 1})}>{strat.summary}</p>
+      {filteredStrats.map((strat, i) => {
+        return <div key={"strat_" + i} className="col-12 lg:col-4 strat-preview-div">
+          <p onClick={() => this.setState({ selectedStrat: strat, accordionIndex: 1 })}>{strat.summary}</p>
           {this.renderStratGif(strat)}
         </div>
       })}
     </div>
   }
 
-  renderStratDetails(): JSX.Element{
-    if(!this.state.selectedStrat) return <div/>;
+  renderStratDetails(): JSX.Element {
+    if (!this.state.selectedStrat) return <div />;
     let strat = this.state.selectedStrat;
     return <div className="grid strat-details-div">
-        <div className="col-12 lg:col-4">
-          {this.renderStratGif(strat)}
-        </div>
-        <div className="col-12 lg:col-6">
-          <p><b>Summary:</b> {strat.summary}</p>
-          <p><b>Category:</b> {strat.categories.map(c=>c.label).join(', ')}</p>
-          <p><b>Difficulty:</b> {strat.difficulties.map(c=>c.label).join(', ')}</p>                    
-          <p><b>Exit:</b> {this.getLinkById(strat.exit_id)}</p>                    
-        </div>
-        <div className="col-12">
-          {strat.description}
-        </div>
+      <div className="col-12 lg:col-4">
+        {this.renderStratGif(strat)}
+      </div>
+      <div className="col-12 lg:col-6">
+        <p><b>Summary:</b> {strat.summary}</p>
+        <p><b>Category:</b> {strat.categories.map(c => c.label).join(', ')}</p>
+        <p><b>Difficulty:</b> {strat.difficulties.map(c => c.label).join(', ')}</p>
+        <p><b>Exit:</b> {this.getLinkById(strat.exit_id)}</p>
+      </div>
+      <div className="col-12">
+        {strat.description}
+      </div>
     </div>
   }
 
-  renderStratGif(strat: Strat){
-    return  <div className="strat-container">
-    <div className="strat-dummy"></div>
-    <div className="strat-wrapper">
-        <iframe src={strat.gif+'?autoplay=0&controls=0'}  height='100%' width='100%' />
+  renderStratGif(strat: Strat) {
+    return <div className="strat-container">
+      <div className="strat-dummy"></div>
+      <div className="strat-wrapper">
+        <iframe src={strat.gif + '?autoplay=0&controls=0'} height='100%' width='100%' />
+      </div>
     </div>
-  </div>
   }
 
   renderStratsSection() {
@@ -216,10 +216,7 @@ export default class Room extends React.Component<ComponentProps, ComponentState
 
       <div className="room">
         <Panel>
-          <div className="grid">
-            {this.renderRoomImage()}
-            {this.renderRoomData()}
-          </div>
+          <RoomDetails room={this.roomLogic.room} chapter={this.roomLogic.chapter} checkpoint={this.roomLogic.checkpoint} side={this.roomLogic.side}/>
           {this.renderStratsSection()}
 
         </Panel>
