b17d0982d4b8203a215fe3fdd5f677354bbc8d48
cobo105322 <danielociruelo@gmail.com>
2022-04-24 11:11:15 +0200

    added headers to allow CORS while on local enviroment

--- server/config/config.php
+++ server/config/config.php
@@ -4,6 +4,18 @@ $config = array(
 );
 
 if($config["env"]=="local"){
+    //ALLOW CORS
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Credentials: true');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method == "OPTIONS") {
+        die();
+    }
+
+
     $dbconfig = array(
         "servername"=>"",
         "user"=>"",
