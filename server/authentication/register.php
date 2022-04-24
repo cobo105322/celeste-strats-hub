ff963287b5a845fcd2bac17925709fec7fb8da30
cobo105322 <danielociruelo@gmail.com>
2022-04-24 11:12:31 +0200

    changed param name of user to name on register

--- server/authentication/register.php
+++ server/authentication/register.php
@@ -3,7 +3,7 @@
 include_once('../config/config.php');
 include_once('../utils/dbutils.php');
 
-if(!isset($_POST["user"]) || !isset($_POST["password"]) || !isset($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+if(!isset($_POST["name"]) || !isset($_POST["password"]) || !isset($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
     header("Status: 404 Wrong Parameters");
     header("HTTP/1.1 404 Wrong Parameters");
     die();
@@ -21,7 +21,7 @@ $conn->select_db($dbconfig["dbname"]);
 $password = hash("sha256", $_POST["password"]);
 
 $stmt = $conn->prepare("INSERT INTO user (name, password, email) VALUES (?, ?, ?)");
-$stmt -> bind_param("sss", $_POST["user"], $password, $_POST["email"]);
+$stmt -> bind_param("sss", $_POST["name"], $password, $_POST["email"]);
 $stmt->execute();
 
 echo $conn->insert_id;
