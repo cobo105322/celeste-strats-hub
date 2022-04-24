2d14af866ba1ee0b91fb66675510c6c7112ab298
cobo105322 <danielociruelo@gmail.com>
2022-04-24 11:13:37 +0200

    added user id and previous linked strat to room submit endpoint

--- server/api/submitRoomStrat.php
+++ server/api/submitRoomStrat.php
@@ -3,14 +3,19 @@
 include_once('../config/config.php');
 include_once('../utils/dbutils.php');
 
+session_start();
+if(!$_SESSION["user"]){
+    http_response_code(403);
+    die();
+}
 //Check if user is logged in
 //TODO
 
 if(!isset($_POST["categories"]) || !isset($_POST["difficulties"]) || !isset($_POST["summary"]) || !isset($_POST["description"]) || !isset($_POST["gif"])
     || !isset($_POST["room_id"])  || !isset($_POST["entry_id"])  || !isset($_POST["exit_id"]))
 {
-    header("Status: 404 Not Found");
-    header("HTTP/1.1 404 Not Found");
+    header("Status: 404 Missing parameters");
+    header("HTTP/1.1 404 Missing parameters");
     die();
 }
 
@@ -28,11 +33,11 @@ $conn = new mysqli($dbconfig["servername"], $dbconfig["user"], $dbconfig["passwo
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+$user = $_SESSION["user"];
 $conn->select_db($dbconfig["dbname"]);
 
-$stmt = $conn->prepare("INSERT INTO strat (summary, description, gif, room_id, entry_id, exit_id) VALUES (?, ?, ?, ?, ?, ?)");
-$stmt -> bind_param("ssssss", $_POST["summary"], $_POST["description"], $_POST["gif"], $_POST["room_id"], $_POST["entry_id"], $_POST["exit_id"]);
+$stmt = $conn->prepare("INSERT INTO strat (summary, description, gif, room_id, entry_id, exit_id, user_id, previous_strat_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt -> bind_param("ssssssis", $_POST["summary"], $_POST["description"], $_POST["gif"], $_POST["room_id"], $_POST["entry_id"], $_POST["exit_id"], $user["id"],  $_POST["previous_strat"]);
 $stmt->execute();
 
 $id = $conn->insert_id;
@@ -55,6 +60,8 @@ foreach($difficulties as $idDifficulty){
     $stmt -> bind_param("ii", $id, $idDifficulty);
     $stmt -> execute();
 }
+
+
 
 $conn->close();
 
