39793207c5c4f3371cba7830da36c64d87268c9c
cobo105322 <danielociruelo@gmail.com>
2022-04-24 11:16:38 +0200

    Temporary disabled tags attribute, added several new attributes

--- client/src/models/interfaces/Strat.ts
+++ client/src/models/interfaces/Strat.ts
@@ -4,11 +4,15 @@ export interface Strat{
     id: number,
     categories: Category[],
     difficulties: Difficulty[],    
-    tags: string[],
+    // tags: string[],
     summary: string,
     description: string,
     gif: string,
     room_id: string,
     entry_id: string,
-    exit_id: string
+    exit_id: string,
+    user_id: number,
+    user: string,
+    time: number,
+    previous_strat: number
 }
