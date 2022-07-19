2dcf551cfac3b6e77f4d8c832a9677d21105141f
cusher <30704914+cusher@users.noreply.github.com>
2022-07-18 19:41:50 -1000

    Add proxy config

--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
 from markupsafe import escape
+from werkzeug.middleware.proxy_fix import ProxyFix
 
+from config import PROXIES
 from database.repository import get_strats_for_room
 
 app = Flask(__name__)
+
+if PROXIES:
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=PROXIES, x_proto=PROXIES, x_host=PROXIES, x_prefix=PROXIES
+    )
 
 
 @app.get('/strats/<chapter>/<room>')
--- server/config.py
+++ server/config.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 DATA_DIR = Path('../data/')
+PROXIES = 1
